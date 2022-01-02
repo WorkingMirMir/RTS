30b1acd40f2cbdde4d199e34218da882ddc0b01f
workingMirMir <ilwoongyang0622@gmail.com>
2022-01-02 19:52:50 +0900

    Selectable FunctionName change

--- Source/RTS/Interface/Selectable.h
+++ Source/RTS/Interface/Selectable.h
@@ -22,6 +22,6 @@ class RTS_API ISelectable
 
 	// Add interface functions to this class. This is the class that will be inherited to implement this interface.
 public:
-	virtual void OnSelected() = 0;
-	virtual void OnNotSelected() = 0;
+	virtual void StartSelect() = 0;
+	virtual void StopSelect() = 0;
 };
